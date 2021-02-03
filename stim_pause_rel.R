0c750764248252b4fe3f170c626f2c120594a629
mjg54 <guertinmj@gmail.com>
2021-02-03 16:28:43 -0500

    Update stim_pause_rel.R

--- stim_pause_rel.R
+++ stim_pause_rel.R
@@ -1,43 +1,42 @@
-
 #initial conditions are:
 #pause.peak 
 #body.peak
-krel.stim=0.2
-result.stimulate.pause.release = ode(c(P = pause.peak , B = body.peak),
-             times = times, 
+krel.stim=krel*7
+kinit.stim = kinit*4
+result.stimulate.pause.release = ode(c(P = pause.peak , B1 = body.peak, B2 = body.peak, B3 = body.peak),
+             times = c(0,100), 
              func = density.prime,
-             parms = c(kinit=kinit,
+             parms = c(kinit=kinit.stim,
                         kpre=kpre,
-                        kterm=kterm,
-                        krel=krel.stim,
-                        lp = lp,
-                        lb = lb))
+                        kelong=kelong,
+                        krel=krel.stim))
 
 result.stimulate.pause.release = data.frame(result.stimulate.pause.release)
+tail(result.stimulate.pause.release)
 
 #plot in lattice
 plot.changes.wrt(result.stimulate.pause.release,
                  filename = 'dynamic_pro_model_stim_pause_release')
 
-plot.changes.wrt(data.frame(ode(y = c(P = pause.peak , B = body.peak),
-             times = seq(0,10000, by = 1),
+
+plot.changes.wrt(data.frame(ode(y = c(P = pause.peak , B1 = body.peak, B2 = body.peak, B3 = body.peak),
+             times = c(0,100),
              func = density.prime,
-             parms = c(kinit=kinit,
+             parms = c(kinit=kinit.stim,
                         kpre=kpre,
-                        kterm=kterm,
-                        krel=krel.stim,
-                        lp = lp,
-                        lb = lb))), 'dynamic_pro_model_stim_pause_PGBdensities_ss')
+                        kelong=kelong,
+                        krel=krel.stim))), 'dynamic_pro_model_stim_pause_PGBdensities_ss')
 
 #plot the steady state profile as well.
-pause.peak.stim = kinit/(kpre + krel.stim) 
-body.peak.stim = (((lp/lb)*krel.stim)*pause.peak.stim)/kterm
+pause.peak.stim = kinit.stim/(kpre + krel.stim) 
+body.peak.stim = ((krel.stim)*pause.peak.stim)/kelong
 steady.state.stim.pro = dynamic.pro.profile(input = data.frame(cbind(22, pause.peak.stim, body.peak.stim)))
 
 
 
 #prepare the simulated composites
-dynamic.pro.stimulate.pause.release = dynamic.pro.profile(input = result.stimulate.pause.release)
+dynamic.pro.stimulate.pause.release = dynamic.pro.profile(input = result.stimulate.pause.release,
+                                                          tau = 40)
 
 #plot composites in lattice
 plot.pro.simulation.composites(dynamic.pro.stimulate.pause.release,
@@ -46,4 +45,3 @@ plot.pro.simulation.composites(dynamic.pro.stimulate.pause.release,
 #steady state composite upon stimuating pause release
 plot.pro.simulation.composites(steady.state.stim.pro,
              filename = 'steady_state_pro_model_stim_pause_density') 
-
